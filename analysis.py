daab0516bd554aa872e0e7a92a9833dcee39e9c4
Stuart Saint <stuartsaint242@gmail.com>
2022-04-23 21:36:15 +0100

    Addition: core project goals

--- analysis.py
+++ analysis.py
@@ -1,1 +1,8 @@
 # Main file for performing analysis
+
+# 1. Output a summary of each variable to a single text file
+# 2. Save a histogram of each variable to png files
+# 3. Output a scatter plot for each pair of variables
+# 4. Performs any other analysis you think is appropriate
+# The last point is what will make the project stand out, the first three are the minimum requirements
+# Remember to comment and document all processes to show understanding of each step, especially where you may be following a tutorial
