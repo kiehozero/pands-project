fea52f007210772b8ef9763baa63c16e1a5836d6
Stuart Saint <stuartsaint242@gmail.com>
2022-04-23 21:41:01 +0100

    Messing with basics of np

--- analysis.py
+++ analysis.py
@@ -6,3 +6,12 @@
 # 4. Performs any other analysis you think is appropriate
 # The last point is what will make the project stand out, the first three are the minimum requirements
 # Remember to comment and document all processes to show understanding of each step, especially where you may be following a tutorial
+
+import numpy as np
+import csv
+
+with open("iris.csv", "r") as d:
+    sample_data = list(csv.reader(d, delimiter=","))
+
+sample_data = np.array(sample_data)
+print(sample_data)
