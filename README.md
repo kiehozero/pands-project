1e65abf186ca650cca96a73c5db93b2e856bb49a
Stuart Saint <stuartsaint242@gmail.com>
2022-04-24 20:42:03 +0100

    More messing with pandas tutorials, working out how to filter arrays

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ A repository for the final project in GMIT Programming and Scripting 2022
 # Methodology and working notes
 
 # Bugfixes
+1. The first bug I encountered was when I was attempting to plot from a CSV file. the csv.reader() function was bringing in the lengths and widths as strings instead of floating-point numbers, so it wasn't plotting the points in value order, but rather in the order that each entry was within the CSV file. I didn't find an exact solution to rectify this in csv.reader(), but going to a basic [matplotlib tutorial](https://www.tutorialspoint.com/plot-data-from-csv-file-with-matplotlib) provided the read_csv() that corrected the problem.
 
 # References
 
 For isolating specific columns in a numPy array: https://www.statology.org/numpy-get-column/
+For reading CSV files in matplotlib: https://www.tutorialspoint.com/plot-data-from-csv-file-with-matplotlib
+For plotting items by group: https://www.w3schools.com/python/matplotlib_scatter.asp
+For filtering with pandas: https://www.tutorialspoint.com/filter-the-rows-python-pandas
--- analysis.py
+++ analysis.py
@@ -9,15 +9,70 @@
 
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
-iris_csv = list(csv.reader(open("iris.csv", "r"), delimiter=","))
+iris_csv = pd.read_csv("iris.csv")
 
 iris_csv = np.array(iris_csv)
 # selects only PL and Species columns
-pet_len = iris_csv[:, [2,4]]
-print(pet_len)
-# selects all data for Virginica species
-for row in iris_csv:
-    if row[4] == "virginica":
-        print(row)
+petal = iris_csv[1:, [2,3,4]]
+# selects all data for Virginica species, need appending to a list to use as data points, make into a function, loop through species name to
+# create unique dataset for each, then another function that loops through each dataset to create scatters to overlay each other
+test = []
+for row in petal:
+    if row[2] == "virginica":
+        test.append(list(row))
+
+testarr = np.array(test)
+
+
+
+print(testarr) 
+filter_arr = petal[:,2]=="virginica"
+
+print(filter_arr)
+
+x_axis = testarr[:,0]
+y_axis = testarr[:,1]
+
+# can plot multiple items in one file
+
+plt.scatter(x_axis,y_axis)
+plt.show()
+
+finalfilter = petal[filter_arr]
+
+print(finalfilter)
+
+# file to create summary of sepal_length
+# number of records, max of each, min of each, mean of each, printout of all items
+
+# file to create summary of sepal_width
+# number of records, max of each, min of each, mean of each, printout of all items
+
+# file to create summary of petal_length
+# number of records, max of each, min of each, mean of each, printout of all items
+
+# file to create summary of petal_width
+# number of records, max of each, min of each, mean of each, printout of all items
+
+# histogram sepal_length
+
+# histogram sepal_width
+
+# histogram petal_length
+
+# histogram petal_width
+
+# scatter sepal_l vs sepal_w
+
+# scatter sepal_l vs petal_l
+
+# scatter sepal_l vs petal_w
+
+# scatter petal_l vs petal_w
+
+# scatter petal_l vs sepal_w
+
+# scatter sepal_w vs petal_w
