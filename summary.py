a4fe6dc7c23db466b003ac94b6597da3801fa1d5
Stuart Saint <stuartsaint242@gmail.com>
2022-04-27 22:54:53 +0100

    Planning: notes for final project plan

---
+++ summary.py
@@ -0,0 +1,12 @@
+
+# file to create summary of sepal_length
+# number of records, max of each, min of each, mean of each, printout of all items, mnost of these are built-in functions in python, could be worth exploring numpy/math functions
+
+# file to create summary of sepal_width
+# number of records, max of each, min of each, mean of each, printout of all items
+
+# file to create summary of petal_length
+# number of records, max of each, min of each, mean of each, printout of all items
+
+# file to create summary of petal_width
+# number of records, max of each, min of each, mean of each, printout of all items
