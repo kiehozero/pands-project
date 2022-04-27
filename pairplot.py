86f4b635cd3e3e7acb3f5bf85ee38a87c7468653
Stuart Saint <stuartsaint242@gmail.com>
2022-04-27 22:53:18 +0100

    Addition: filepath and savefig function, README information

--- pairplot.py
+++ pairplot.py
@@ -1,4 +1,7 @@
+# A program that uses seaborn to re-create an example of iris data analysis found on the dataset's Wikipedia page (link in README)
+
 import matplotlib.pyplot as plt
+import os.path
 import pandas as pd
 import seaborn as sns
 
@@ -7,4 +10,7 @@ iris_csv = pd.read_csv("iris.csv")
 
 sns.pairplot(data=iris_csv, hue="species")
 plt.legend()
-plt.show()
+filename = "pairplot.png"
+path = "./images/"
+filepath = os.path.join(path, filename)
+plt.savefig(filepath)
